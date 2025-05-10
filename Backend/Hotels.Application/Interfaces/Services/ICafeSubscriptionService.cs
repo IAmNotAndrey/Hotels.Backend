9d7f502523cc87e936e940ca08b9f950a3a40d6d
IAmNotAndrey <permandrey2000@mail.ru>
2025-05-10 20:34:18 +0500

    CafeSubscriptionRepository Split

---
+++ Backend/Hotels.Application/Interfaces/Services/ICafeSubscriptionService.cs
@@ -0,0 +1,8 @@
+﻿using Hotels.Domain.Entities.PaidServices;
+
+namespace Hotels.Application.Interfaces.Services;
+
+public interface ICafeSubscriptionService
+{
+    Task<CafeSubscription> CreateAsync(Guid cafeId, Guid paidServiceId);
+}
--- Backend/Hotels.Persistence/Repositories/CafeSubscriptionRepo.cs
+++ Backend/Hotels.Persistence/Repositories/CafeSubscriptionRepo.cs
@@ -1,7 +1,5 @@
 ﻿using AutoMapper;
 using Hotels.Application.Dtos.Subscriptions;
-using Hotels.Application.Exceptions;
-using Hotels.Domain.Entities;
 using Hotels.Domain.Entities.PaidServices;
 using Hotels.Persistence.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -11,33 +9,13 @@ namespace Hotels.Persistence.Repositories;
 public class CafeSubscriptionRepo : ICafeSubscriptionRepo
 {
     private readonly IMapper _mapper;
-    private readonly IGenericRepo<Cafe, Guid> _cafeRepo;
-    private readonly IGenericRepo<CafeTimeLimitedPaidService, Guid> _cafeTLPSRepo;
     private readonly IGenericRepo<CafeSubscription, Guid> _repo;
 
     public CafeSubscriptionRepo(IMapper mapper,
-                                IGenericRepo<Cafe, Guid> cafeRepo,
-                                IGenericRepo<CafeTimeLimitedPaidService, Guid> cafeTLPSRepo,
                                 IGenericRepo<CafeSubscription, Guid> repo)
     {
         _mapper = mapper;
-        _cafeRepo = cafeRepo;
-        _cafeTLPSRepo = cafeTLPSRepo;
         _repo = repo;
-    }
-
-    public async Task<CafeSubscription> CreateAsync(Guid cafeId, Guid paidServiceId)
-    {
-        if (!await _cafeRepo.ExistsAsync(cafeId))
-        {
-            throw new EntityNotFoundException($"{nameof(Cafe)} wasn't found.");
-        }
-        if (!await _cafeTLPSRepo.ExistsAsync(paidServiceId))
-        {
-            throw new EntityNotFoundException($"{nameof(CafeTimeLimitedPaidService)} wasn't found.");
-        }
-        CafeSubscription sub = new() { CafeId = cafeId, PaidServiceId = paidServiceId };
-        return await _repo.AddAsync(sub);
     }
 
     public async Task<IEnumerable<CafeSubscriptionDto>> GetDtosIncludedByCafeAsync(Guid cafeId)
